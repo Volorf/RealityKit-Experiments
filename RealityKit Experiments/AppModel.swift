966feef7ea50a9cd9a81a889609a7e7895013021
Oleg Frolov <frolololeg@gmail.com>
2025-04-18 20:10:23 +0100

    Initial Commit

---
+++ RealityKit Experiments/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  RealityKit Experiments
+//
+//  Created by Oleg Frolov on 18/04/2025.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ RealityKit Experiments/RealityKit_ExperimentsApp.swift
@@ -0,0 +1,34 @@
+//
+//  RealityKit_ExperimentsApp.swift
+//  RealityKit Experiments
+//
+//  Created by Oleg Frolov on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct RealityKit_ExperimentsApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
